8f982bcaec4c8a8a62d388554187d98817eb055f
Prathik Ramesh <prathik.ramesh@knights.ucf.edu>
2020-10-06 15:22:56 -0400

    Added import statement.

--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import requests
 import json
 import csv
 import sys
+import os
 from dotenv import load_dotenv
 from geocodio import GeocodioClient
 from flask import Flask, render_template, request
